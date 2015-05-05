cbf974b5a55f0bae299a1001826f1f0b58488959
Ammu <Ammu@Ammu-PC>
2015-05-05 18:38:37 +0530

    c1

---
+++ proj2/src/main/java/com/vklalitha/tools/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.vklalitha.tools;
+
+public class HelloWorld {
+	public void Hello() {
+		System.out.println("Hello Ammu");	
+	}
+
+}
---
+++ proj2/src/test/java/com/vklalitha/tools/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package com.vklalitha.tools;
+
+import org.testng.annotations.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void HelloTest(){
+		HelloWorld H1 = new HelloWorld();
+	
+		H1.Hello();
+	}
+	
+}
